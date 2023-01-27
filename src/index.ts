e7909e49e557785156601cf34145506bb1093a81
zerodegress <zerodegress@outlook.com>
2023-01-27 23:25:55 +0800

    upgrade to 2.0 with completely different builder system.

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import os from "os";
 import walkdir from "walkdir";
 import toml from "toml";
 import mkdirp from "mkdirp";
-import { Command } from "commander";
-import { ClassicSource } from "rw-toml";
-import { implCommonToml, StandardIni } from "rw-toml/lib/config";
-import { err, Result } from "rw-toml/lib/result";
-import { ClassicBuilderSync, ClassicTargetFileType } from "rw-toml/lib/builder";
+import { Command } from "@commander-js/extra-typings";
 const program = new Command();
+
+import { build, RwTomlConverterContext } from "rw-build-util/lib/builder/toml";
+import { RwToml } from "rw-build-util/lib/data/toml";
+import * as rwtoml from "rw-build-util/lib/data/toml";
+import * as rwini from "rw-build-util/lib/data/ini";
+import { none } from "rw-build-util/lib/util/optional";
+import { Path } from "rw-build-util/lib/util/path";
 
 async function readFileAsync(path: fs.PathOrFileDescriptor): Promise<Buffer> {
     return new Promise((resolve, reject) => fs.readFile(path, (error, data) => {
@@ -33,82 +36,99 @@ async function writeFileAsync(file: fs.PathOrFileDescriptor, data: string | Node
     });
 }
 
-async function walkdirAsync(path: string, callback: (path: string, stat: fs.Stats) => void): Promise<string[]> {
-    const paths = await walkdir.sync(path, callback);
-    return paths;
+async function walkdirAsync(path: string, callback: (path: string, stat: fs.Stats) => void): Promise<null> {
+    return new Promise((resolve, reject) => {
+        const emmiter = walkdir(path, callback);
+        emmiter.on('error', () => {
+            reject();
+        });
+        emmiter.on('end', () => {
+            resolve(null);
+        });
+    });
 }
 
-program
-    .name('rw-build')
-    .description('CLI to Rusted Warfare modding build')
-    .version('1.0.0');
+program.name('rwbuild')
+    .version('2.0.0')
+    .description('A CLI tool for building Rusted Warfare mods.WORK IN PROGRESS.');
 
 program.command('build')
-    .description('build the mod from source')
-    .option('-r, --rootdir <rootdir>', 'locates the root directory for source and output', '.')
-    .option('-o, --outputdir <outputdir>', 'locates the output directory for output', 'dist')
-    .option('-s, --sourcedir <sourcedir>', 'locates the source directory', 'src')
-    .action(async (options) => {
-        const rootdir = options.rootdir as string;
-        const outputdir = options.outputdir as string;
-        const sourcedir = options.sourcedir as string;
-        try {
-            let promises: [string, Promise<Buffer>][] = [];
-            let sources: ClassicSource[] = [];
-            const paths = await walkdirAsync(path.join(rootdir, sourcedir), async (pat, stat) => {
-                if(stat.isFile()) {
-                    promises.push([pat, readFileAsync(pat)]);
-                }
-            });
-            for(const [pat, pro] of promises) {
-                try {
-                    const data = await pro;
-                    if(pat.endsWith('.toml')) {
-                        const tom = toml.parse(data.toString());
-                        if(implCommonToml(tom)) {
-                            sources.push(ClassicSource.toml(path.basename(pat), path.relative(path.join(rootdir, sourcedir), path.dirname(pat)), path.relative(path.join(rootdir, sourcedir), pat), tom));
-                        } else {
-                            throw err<null>(new Error(`${pat} is not a CommonToml File`));
-                        }
-                    }
-                } catch(error) {
-                    if(error instanceof Error) {
-                        throw err<null>(error);
-                    } else {
-                        throw error;
-                    }
-                }
+    .option('--outdir <outdir>', 'locate the output directory', 'build')
+    .option('--srcdir <srcdir>', 'locate the source directory', 'src')
+    .option('--rootdir <rootdir>', 'locate the root directory', '.')
+    .action(async ({outdir, srcdir, rootdir}) => {
+        const paths: string[] = [];
+        await walkdirAsync(path.join(rootdir, srcdir), (pat, stat) => {
+            if(stat.isFile()) {
+                paths.push(path.normalize(pat));
             }
-            new ClassicBuilderSync(rootdir, sourcedir, outputdir, sources).buildAllSync().ok(async (targets) => {
-                for(const target of targets) {
-                    if(target.targetFile.type == ClassicTargetFileType.INI) {
-                        let ini = '';
-                        const content = target.targetFile.content as StandardIni;
-                        for(const sec in content) {
-                            ini += `[${sec}]` + '\n';
-                            for(const key in content[sec]) {
-                                const value = content[sec][key];
-                                if(value.includes(os.EOL) || value.includes('\n')) {
-                                    ini += key + ':"""' + value + '"""\n';
-                                } else {
-                                    ini += key + ':' + value + '\n';
-                                }
-                            }
-                        }
-                        await mkdirp(path.join(rootdir, outputdir, target.dirname));
-                        await writeFileAsync(path.join(rootdir, outputdir, target.dirname, target.filename), ini);
-                    }
-                }
-            }).err((error) => {throw err<null>(error)});
-        } catch(result) {
-            if(result instanceof Result) {
+        });
+        const tomls: [string, RwToml][] = [];
+        for(const pat of paths) {
+            if(path.extname(pat) == '.toml') {
+                const buffer = await readFileAsync(pat);
+                const result = rwtoml.fromString(buffer.toString());
+                result.ok((toml) => tomls.push([pat, toml]))
                 result.err((error) => {
+                    console.error(buffer.toString());
                     console.error(error);
+                    process.exit(-3);
                 });
-            } else {
-                throw result;
             }
         }
+        const result = build({
+            context: {
+                sources: tomls.map(([pat, toml]) => {
+                    const pathr: Path = path.relative(srcdir, pat).replace(/\.toml/, '').split(path.sep);
+                    return {
+                        path: pathr,
+                        content: toml,
+                        from: none(),
+                        to: none()
+                    };
+                }),
+                targets: [],
+                into(this: RwTomlConverterContext) {
+                    return {
+                        sources: this.sources.map((x) => {
+                            return {
+                                path: x.path,
+                                content: x.content,
+                                from: none(),
+                                to: none(),
+                            };
+                        }),
+                        targets: [],
+                    };
+                }
+            }
+        });
+        result.err((error) => {
+            console.error(error);
+            process.exit(-1);
+        });
+        result.ok(async (targets) => {
+            for(const [pat, ini] of targets.map((x) => {
+                return [path.join(outdir, path.join(...x.path.map((v) => v)) + '.ini'), (() => {
+                    let str = '';
+                    for(const sec in x.content) {
+                        str += `[${sec}]` + '\n';
+                        for(const [key, value] of Object.entries(x.content[sec])) {
+                            str += `${key}:${value}` + '\n';
+                        }
+                    }
+                    return str;
+                })()]
+            })) {
+                await mkdirp(path.dirname(pat));
+                try {
+                    await writeFileAsync(pat, ini);
+                } catch (error) {
+                    console.error(error);
+                    process.exit(-2);
+                }
+            }
+        });
     });
 
 program.parse();
