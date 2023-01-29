04d133920ccc77e6a9bb8d4d5c9c19ce70e7c9a5
zerodegress <zerodegress@outlook.com>
2023-01-29 19:49:00 +0800

    Added load config file support.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import path from "path";
-import fs from "fs";
+import fs from "fs-extra";
 import os from "os";
 import walkdir from "walkdir";
 import mkdirp from "mkdirp";
+import toml from "toml";
+import dclone from "clone";
+import merge from "webpack-merge";
+import minimatch from "minimatch";
 import { Command } from "@commander-js/extra-typings";
 const program = new Command();
 
@@ -14,26 +18,20 @@ const { build } = tomlbuilder;
 const { optional, some, none } = opt;
 const { ok, err } = result;
 
-async function readFileAsync(path: fs.PathOrFileDescriptor): Promise<Buffer> {
-    return new Promise((resolve, reject) => fs.readFile(path, (error, data) => {
-        if(error) {
-            reject(error);
-        } else {
-            resolve(data);
-        }
-    }));
+type FilterRule = {
+    path?: {from: string, to: string},
+    code?: {from: RwToml, to: RwToml}
 }
 
-async function writeFileAsync(file: fs.PathOrFileDescriptor, data: string | NodeJS.ArrayBufferView): Promise<null> {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, data, (error) => {
-            if(error) {
-                reject(error);
-            } else {
-                resolve(null);
-            }
-        })
-    });
+type FilterConfig = {
+    include?: string[],
+    exclude?: string[],
+    rules?: FilterRule[],
+}
+
+type ConfigToml = {
+    filters?: (string | FilterConfig)[],
+    imports?: string[]
 }
 
 async function walkdirAsync(path: string, callback: (path: string, stat: fs.Stats) => void): Promise<null> {
@@ -48,6 +46,33 @@ async function walkdirAsync(path: string, callback: (path: string, stat: fs.Stat
     });
 }
 
+async function serializeConfigToml(tomlPath: string): Promise<ConfigToml> {
+    return fs.readFile(tomlPath).then((result) => {
+        return toml.parse(result.toString());
+    }).catch((error) => {
+        console.error(`config file ${tomlPath} cannot be read.`);
+        console.error(err);
+        process.exit(-4);
+    }).then(async (tom) => {
+        let conf = tom as ConfigToml;
+        if(conf.imports) {
+            const impProms: [Promise<ConfigToml>, string][] = [];
+            for(const imp of conf.imports) {
+                impProms.push([serializeConfigToml(path.join(path.dirname(tomlPath), imp)), imp]);
+            }
+            for(const impProm of impProms) {
+                const impConf = await impProm[0];
+                const imp = impProm[1];
+                if(impConf.imports) {
+                    impConf.imports = impConf.imports.map((x) => path.join(path.dirname(tomlPath), imp, path.dirname(imp), x));
+                }
+                conf = merge(conf, impConf);
+            }
+        }
+        return conf;
+    })
+}
+
 program.name('rwbuild')
     .version('2.0.0')
     .description('A CLI tool for building Rusted Warfare mods.WORK IN PROGRESS.');
@@ -56,17 +81,32 @@ program.command('build')
     .option('--outdir <outdir>', 'locate the output directory', 'build')
     .option('--srcdir <srcdir>', 'locate the source directory', 'src')
     .option('--rootdir <rootdir>', 'locate the root directory', '.')
-    .action(async ({outdir, srcdir, rootdir}) => {
+    .option('--config <config-file>', 'locate the config file', 'rwbuild.config.toml')
+    .action(async ({outdir, srcdir, rootdir, config}) => {
         const paths: string[] = [];
-        await walkdirAsync(path.join(rootdir, srcdir), (pat, stat) => {
+        const walkProm = walkdirAsync(path.join(rootdir, srcdir), (pat, stat) => {
             if(stat.isFile()) {
                 paths.push(path.normalize(pat));
             }
         });
         const tomls: [string, RwToml][] = [];
+        const ignorePaths: string[] = [path.join(rootdir, config)];
+        const filterConfigs: (FilterConfig | string)[] = [];
+        await serializeConfigToml(path.join(rootdir, config)).then((conf) => {
+            if(conf.imports) {
+                conf.imports.forEach((x) => ignorePaths.push(x));
+            }
+            if(conf.filters) {
+                conf.filters.forEach((x) => filterConfigs.push(x));
+            }
+        });
+        await walkProm;
         for(const pat of paths) {
+            if(path.basename(pat) == config || ignorePaths.find((v) => minimatch(pat, v))) {
+                continue;
+            }
             if(path.extname(pat) == '.toml') {
-                const buffer = await readFileAsync(pat);
+                const buffer = await fs.readFile(pat);
                 const result = rwtoml.fromString(buffer.toString());
                 result.ok((toml) => tomls.push([pat, toml]))
                 result.err((error) => {
@@ -113,7 +153,7 @@ program.command('build')
             })) {
                 await mkdirp(path.dirname(pat));
                 try {
-                    await writeFileAsync(pat, ini);
+                    await fs.writeFile(pat, ini);
                 } catch (error) {
                     console.error(error);
                     process.exit(-2);
